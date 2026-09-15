aec6631ffcab589423f52c7f536ba65c07f585de
李贵江 <599575094@qq.com>
2026-09-15 10:28:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
-  server: { host: '0.0.0.0', port: 5173 }
+  server: { localhost: '0.0.0.0', port: 5173 }
 })
